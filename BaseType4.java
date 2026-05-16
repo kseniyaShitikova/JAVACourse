47e66ab5017311ed31a03d1915ccf16d2539830d
kseniyaShitikova <ksen.shitidenn@gmail.com>
2026-05-16 13:24:33 +0300

    Create BaseType4.java
    Базовые типы данных и литералы 4

---
+++ BaseType4.java
@@ -0,0 +1,14 @@
+
+public class BaseType4 {
+   
+    public static void main(String[] args) {
+        String str = "Результат: ";
+        int num = 42;
+        double pi = 3.14;
+        boolean flag = true;
+        String result = str + num + ", " + pi + ", " + flag;
+        
+        System.out.println(result);
+    }
+
+}
